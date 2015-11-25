950eefd06a88a93d858b06233dfa560585b22f95
nischalshrestha <bitsorific@gmail.com>
2015-11-24 23:31:38 -0500

    Playing with webapp2 and Jinja2 to dynamically populate table in Match page

--- models.py
+++ models.py
@@ -6,7 +6,8 @@ Defines models for persisting and querying data on Image(s) related to Album(s)
 from google.appengine.ext import ndb
 
 class Image(ndb.Model):
-	# correct = ndb.BooleanProperty()
+	correct = ndb.BooleanProperty()
+	title = ndb.StringProperty()
 	image = ndb.BlobProperty()
 
 class Album(ndb.Model):
--- recognize.py
+++ recognize.py
@@ -1,22 +1,51 @@
 import webapp2
 
+import os
+import jinja2
+
 from models import Album
 from models import Image
 
-# class MainPage(webapp2.RequestHandler):
-#     def get(self):
-#         self.response.headers['Content-Type'] = 'text/plain'
-#         self.response.write('Hello, World!')
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader('templates'),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
-class Guestbook(webapp2.RequestHandler):
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+    	# TODO: Eventually query the list of Games, and their Albums to dynamically populate page
+    	# Eg values
+    	template_values = {
+            'title1': 'Orange Tabbies!',
+            'category1': 'Cats',
+            'title2': 'German Shepherds!',
+            'category2': 'Dogs'
+        }
+        template = JINJA_ENVIRONMENT.get_template('match.html')
+      	self.response.write(template.render(template_values))
+
+class ImageStore(webapp2.RequestHandler):
     def post(self):
-		img1 = Image(image=self.request.get('img_1'))
-		img2 = Image(image=self.request.get('img_2'))
-		img3 = Image(image=self.request.get('img_3'))
-		album = Album(title="Cats", images=[img1, img2, img3])
+		img1 = Image()
+		img1.image = self.request.get('img_1')
+		if self.request.get("correct_answer_1"):
+			img1.title = "correct_answer"
+			img1.correct = True
+		else:
+			img1.title = "incorrect_answer_1"
+			img1.correct = False
+
+		# img2 = Image(image=self.request.get('img_2'), title=self.request.title)
+		# img3 = Image(image=self.request.get('img_3'), title=self.request.title)
+		# album = Album(title="Cats", images=[img1, img2, img3])
 		# album.put()
-		album.put()
+		# album.put()
+		img1_key = img1.put()
+		# img2.put()
+		# img3.put()
+		# self.response.out.write("key: "+str(img1_key.id())+" title: "+img1.title+" correct? "+self.request.get("correct_answer_1"))
 		self.redirect(self.request.referer)
 
-app = webapp2.WSGIApplication([('/upload', Guestbook)],
-                              debug=True)
+app = webapp2.WSGIApplication([('/upload', ImageStore), 
+							   ('/match', MainPage)],
+								debug=True)
